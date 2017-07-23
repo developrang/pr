d84702b1b8e71482680c5323a036e7860b65b1dd
Pandurang Nanaware <prangnanaware@gmail.com>
2017-07-23 18:08:46 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule} from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     HeaderComponent,
+    MainNavComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
